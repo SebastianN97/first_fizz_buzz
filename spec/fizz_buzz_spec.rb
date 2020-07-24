913fb8669311b88421b5178e060376c6e5c3160d
SebastianN97 <sebbe132132@gmail.com>
2020-07-24 14:05:22 +0200

    finished with Wrap it up

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,6 +1,7 @@
 require './lib/fizz_buzz'
 
 describe 'fizz_buzz' do
+
     it 'returns 1 if number is 1' do
         expect(fizz_buzz(1)).to eq 1 
     end
@@ -9,15 +10,15 @@ describe 'fizz_buzz' do
         expect(fizz_buzz(3)).to eq 'fizz'
     end
 
+    it "returns 'buzz' if number is divisable by 5" do
+        expect(fizz_buzz(5)).to eq 'buzz'
+    end
+
+    it "returns 'fizz buzz' if number is divisable by 15" do
+        expect(fizz_buzz(15)).to eq 'fizz buzz'
+    end
+
 end
-
-
-
-
-
-
-
-
 
 
 
